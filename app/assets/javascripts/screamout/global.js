c619608f6f9edd4a81291e3ec4ef3702e10ab235
Ashish <aashishpande944@gmail.com>
2020-06-13 09:59:55 +0530

    Update Filter JS to v2.1.0 and Fix UI

--- app/assets/javascripts/screamout/global.js
+++ app/assets/javascripts/screamout/global.js
@@ -1,6 +1,4 @@
 loadTags = function(){
-  console.log($('.search-query'));
-
 $('.search-query').on('change', function(e) {
   var selected_vals = $.map($('.search-query').select2('data'), function(e, i){return e.text}).join(',')
   $('#content_tags').val(selected_vals);
@@ -15,9 +13,6 @@ function filterInit(){
   var template = $("#template").html(); //got template
   var view = function(conc){
     conc.tags = conc.tags_array.join(',')
-    console.log(conc);
-    console.log(current_user);
-    console.log(conc.user_id);
     if(current_user == conc.user_id || current_user_role == 'Admin' || current_user_role == 'Super Admin')
       conc.valid_user = true;
     else if(current_user == "")
@@ -27,30 +22,24 @@ function filterInit(){
     return Mustache.render(template, conc);
   }
 
-  var filter_type_functions = {
-    tag: function(content_input_value, tag){
-    return content_input_value.indexOf(tag) > -1 ? true : false;
-    }
-  };
-
-  var filter_callbacks = {
-    after_filter: function(result){
-      console.log(result.length)
-      if(result.length == 0){
-        $('.fjs-ele').css('display', 'block');
-      }    
+  var afterFilter = function(result){
+    if(result.length == 0){
+      $('.fjs-ele').css('display', 'block');
     }
   }
 
   var settings = {
-    filter_criteria:{
-      tags: ['#content_tags .EVENT.change .TYPE.tag' ,'tags_array']
-    },
-    callbacks: filter_callbacks,
-    filter_on_init: false,
-    filter_types: filter_type_functions,
-    and_filter_on: false,
-    //search: { input: '#content_tags1', search_in: '.tags', min_length: 4}
+    template: template,
+    view: view,
+    criterias: [{
+      field: 'tags_array',
+      ele: '#content_tags',
+      event: 'change',
+      type: 'tag'
+    }],
+    callbacks: {
+      afterFilter: afterFilter 
+    }
   }
-  return FilterJS(conc, "#content_list",view,settings)
+  return FilterJS(conc, "#content_list", settings)
 }
